fecb91c7a20b80fdae24837e787f7d805c8a6363
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-02 11:28:29 -0500

    Fix import statement for better readability

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// Register the main application component
 AppRegistry.registerComponent(appName, () => App);
